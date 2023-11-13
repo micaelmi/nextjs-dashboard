47bbd075ab8c1969b3632ae3aad2d41fd6a506e9
Micael Miranda <micaelmiranda124@gmail.com>
2023-11-13 14:37:30 -0300

    cap 16 - metadata

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,11 @@ import AcmeLogo from "@/app/ui/acme-logo";
 import Link from "next/link";
 import { lusitana } from "./ui/fonts";
 import Image from "next/image";
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default function Page() {
   return (
